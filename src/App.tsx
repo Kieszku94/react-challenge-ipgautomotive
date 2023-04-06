6d679b88e180ce4de246de5d408499ba1e6a6ce9
Kieszku94 <kieszku94@gmail.com>
2023-04-06 10:03:26 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { fetchCity } from "./helpers/fetchCity";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
-import { UserProvider } from "./context/userContext";
 import { useUserAuthenticated } from "./context/userContext";
 
 function App() {
@@ -23,15 +22,13 @@ function App() {
   // }, []);
 
   return (
-    <UserProvider>
-      <Routes>
-        <Route
-          path="/"
-          element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
-        />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </UserProvider>
+    <Routes>
+      <Route
+        path="/"
+        element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
+      />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
