af211fbbbfe3db2e319b96bf715dd8b2f4136bd6
Kieszku94 <kieszku94@gmail.com>
2023-04-06 13:12:52 +0100

    Update City.tsx

--- src/components/City/City.tsx
+++ src/components/City/City.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchCity } from "../../helpers/fetchCity";
 
-const City = () => {
-  return <div>City</div>;
+interface Iprops {
+  city: string;
+}
+
+type Condition = {
+  text: string;
+  icon: string;
+};
+
+type Location = {
+  name: String;
+};
+
+type Current = {
+  condition: Condition;
+  temp_c: Number;
+  humidity: Number;
+};
+
+type CityType = {
+  location: Location;
+  current: Current;
+};
+
+const City = (props: Iprops) => {
+  const [city, setCity] = useState<CityType>();
+
+  useEffect(() => {
+    const getCity = async () => {
+      try {
+        await fetchCity(props.city).then((res) => {
+          console.log(res.data);
+          setCity(res.data);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCity();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return <div>{`${city?.current.temp_c}`}</div>;
 };
 
 export default City;
