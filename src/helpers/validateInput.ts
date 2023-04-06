5882219736e61569ea83a54f2f6806517e06a6c8
Kieszku94 <kieszku94@gmail.com>
2023-04-06 23:53:47 +0100

    Update validateInput.ts

--- src/helpers/validateInput.ts
+++ src/helpers/validateInput.ts
@@ -1,8 +1,11 @@
 const validCityRegex =
   /^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/;
 
+const specialCharactersRegex = /[`!@#$%^&*()_+=[\]{};'"\\|<>?~]/;
+
 export const validateInput = (textboxValue: string): boolean => {
   if (validCityRegex.test(textboxValue) === false) return false;
+  if (specialCharactersRegex.test(textboxValue)) return false;
   if (textboxValue.includes(",")) return false;
   return true;
 };
