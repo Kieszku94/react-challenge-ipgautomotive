346da6001571467f171937e7e478d5a80a13b25e
Kieszku94 <kieszku94@gmail.com>
2023-04-06 15:34:42 +0100

    Update City.tsx

--- src/components/City/City.tsx
+++ src/components/City/City.tsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./City.css";
 import { fetchCity } from "../../helpers/fetchCity";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 interface Iprops {
   city: string;
+  cities: string[];
+  setCities: React.Dispatch<React.SetStateAction<string[]>>;
   index: Number;
 }
 
@@ -27,6 +31,7 @@ type Current = {
 type CityType = {
   location: Location;
   current: Current;
+  id: Number;
 };
 
 const City = (props: Iprops) => {
@@ -47,31 +52,42 @@ const City = (props: Iprops) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
-    <div className="cityContainer">
-      <div className="split">
-        <div className="cityField">City name</div>
-        <div className="cityField">{city?.location.name}</div>
+    <div className="cityWrapper">
+      <div className="cityContainer">
+        <div className="split">
+          <div className="cityField">City name</div>
+          <div className="cityField">{city?.location.name}</div>
+        </div>
+        <div className="split">
+          <div className="cityField">Description of current weather</div>
+          <div className="cityField">{city?.current.condition.text}</div>
+        </div>
+        <div className="split">
+          <div className="cityField">Image of current weather</div>
+          <div className="cityField">{`${city?.current.condition.code}`}</div>
+        </div>
+        <div className="split">
+          <div className="cityField">Current temperature in C</div>
+          <div className="cityField">{`${city?.current.temp_c}`}</div>
+        </div>
+        <div className="split">
+          <div className="cityField">Current humidity</div>
+          <div className="cityField">{`${city?.current.humidity}`}</div>
+        </div>
+        <div className="split">
+          <div className="cityField">Current precipitation</div>
+          <div className="cityField">{`${city?.current.precip_mm}`}</div>
+        </div>
       </div>
-      <div className="split">
-        <div className="cityField">Description of current weather</div>
-        <div className="cityField">{city?.current.condition.text}</div>
-      </div>
-      <div className="split">
-        <div className="cityField">Image of current weather</div>
-        <div className="cityField">{`${city?.current.condition.code}`}</div>
-      </div>
-      <div className="split">
-        <div className="cityField">Current temperature in C</div>
-        <div className="cityField">{`${city?.current.temp_c}`}</div>
-      </div>
-      <div className="split">
-        <div className="cityField">Current humidity</div>
-        <div className="cityField">{`${city?.current.humidity}`}</div>
-      </div>
-      <div className="split">
-        <div className="cityField">Current precipitation</div>
-        <div className="cityField">{`${city?.current.precip_mm}`}</div>
-      </div>
+      <FontAwesomeIcon
+        className="deleteBtn"
+        icon={faTrash}
+        onClick={() => {
+          props.setCities(
+            props.cities.filter((x) => x !== city?.location.name)
+          );
+        }}
+      />
     </div>
   );
 };
