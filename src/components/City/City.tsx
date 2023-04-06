4d5b7f1f1a8b47ac0cf81a4753e1ab0ba1dc6c56
Kieszku94 <kieszku94@gmail.com>
2023-04-06 14:06:25 +0100

    Update City.tsx

--- src/components/City/City.tsx
+++ src/components/City/City.tsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
+import "./City.css";
 import { fetchCity } from "../../helpers/fetchCity";
 
 interface Iprops {
   city: string;
+  index: Number;
 }
 
 type Condition = {
+  code: Number;
   text: string;
   icon: string;
 };
@@ -18,6 +21,7 @@ type Current = {
   condition: Condition;
   temp_c: Number;
   humidity: Number;
+  precip_mm: Number;
 };
 
 type CityType = {
@@ -32,7 +36,7 @@ const City = (props: Iprops) => {
     const getCity = async () => {
       try {
         await fetchCity(props.city).then((res) => {
-          console.log(res.data);
+          res.data.id = props.index;
           setCity(res.data);
         });
       } catch (error) {
@@ -42,7 +46,34 @@ const City = (props: Iprops) => {
     getCity();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return <div>{`${city?.current.temp_c}`}</div>;
+  return (
+    <div className="cityContainer">
+      <div className="split">
+        <div className="cityField">City name</div>
+        <div className="cityField">{city?.location.name}</div>
+      </div>
+      <div className="split">
+        <div className="cityField">Description of current weather</div>
+        <div className="cityField">{city?.current.condition.text}</div>
+      </div>
+      <div className="split">
+        <div className="cityField">Image of current weather</div>
+        <div className="cityField">{`${city?.current.condition.code}`}</div>
+      </div>
+      <div className="split">
+        <div className="cityField">Current temperature in C</div>
+        <div className="cityField">{`${city?.current.temp_c}`}</div>
+      </div>
+      <div className="split">
+        <div className="cityField">Current humidity</div>
+        <div className="cityField">{`${city?.current.humidity}`}</div>
+      </div>
+      <div className="split">
+        <div className="cityField">Current precipitation</div>
+        <div className="cityField">{`${city?.current.precip_mm}`}</div>
+      </div>
+    </div>
+  );
 };
 
 export default City;
