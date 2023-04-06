3bf6d207e7441ae1b7189bbff2825748aad70afd
Kieszku94 <kieszku94@gmail.com>
2023-04-06 23:25:53 +0100

    Update Home.tsx

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,15 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
 import { useUsername } from "../../context/userContext";
-import { getCitiesList } from "../../helpers/getCitiesList";
+import { validateInput } from "../../helpers/validateInput";
+import { toPascalCase } from "../../helpers/toPascalCase";
+import { fetchCity } from "../../helpers/fetchCity";
 import City from "../City/City";
+import { CityType } from "../../types/types";
 
 const Home = (): JSX.Element => {
   const username = useUsername();
   const [textboxValue, setTextboxValue] = useState("");
-  const [cities, setCities] = useState<string[]>([]);
+  const [cities, setCities] = useState<CityType[]>([]);
   const [error, setError] = useState(false);
-  const [maximumError, setMaximumError] = useState(false);
+  const [fetchError, setFetchError] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+  const [duplicateError, setDuplicateError] = useState(false);
+
+  const handleOnClick = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    const validInput = validateInput(textboxValue);
+    for (let city of cities) {
+      if (city.location.name === toPascalCase(textboxValue)) {
+        setDuplicateError(true);
+        return;
+      }
+    }
+    if (validInput) {
+      try {
+        const result = await fetchCity(toPascalCase(textboxValue));
+        setCities((cities) => [...cities, result.data]);
+        setTextboxValue("");
+      } catch (error) {
+        setFetchError(true);
+      }
+    } else {
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    if (cities.length === 5) {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+  }, [cities]);
 
   return (
     <div className="container">
@@ -22,21 +59,39 @@ const Home = (): JSX.Element => {
         <input
           className="textfieldInput"
           type="text"
-          onChange={(e) => setTextboxValue(e.target.value)}
+          onChange={(e) => {
+            setError(false);
+            setFetchError(false);
+            setDuplicateError(false);
+            setTextboxValue(e.target.value);
+          }}
+          value={textboxValue}
         />
         <button
-          className="addToListBtn"
-          onClick={() => {
-            const cts = getCitiesList(textboxValue);
-            if (cts.length > 5) return;
-            setCities(cts);
-          }}
+          className={disabled ? "addToListBtn disabled" : "addToListBtn"}
+          onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+            handleOnClick(e)
+          }
+          disabled={disabled}
         >
           Add
         </button>
       </div>
-      {cities.map((c) => (
-        <City key={c} cities={cities} setCities={setCities} city={c} />
+      {disabled && (
+        <p className="err">The list is full. You cannot add more.</p>
+      )}
+      {duplicateError && (
+        <p className="err">List already contains this city.</p>
+      )}
+      {error && <p className="err">Please provide valid city.</p>}
+      {fetchError && (
+        <p className="err">
+          Couldn't fetch the weather data for {textboxValue}
+        </p>
+      )}
+
+      {cities.map((c: CityType, index) => (
+        <City key={index} cities={cities} setCities={setCities} city={c} />
       ))}
     </div>
   );
