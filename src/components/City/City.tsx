a56fb5bbad4c507e1e04730d6fadda2c1ede2278
Kieszku94 <kieszku94@gmail.com>
2023-04-06 23:25:48 +0100

    Update City.tsx

--- src/components/City/City.tsx
+++ src/components/City/City.tsx
@@ -1,77 +1,76 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./City.css";
-import { fetchCity } from "../../helpers/fetchCity";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faDroplet } from "@fortawesome/free-solid-svg-icons";
+import { faCloudRain } from "@fortawesome/free-solid-svg-icons";
 import { CityType } from "../../types/types";
 
 interface Iprops {
-  city: string;
-  cities: string[];
-  setCities: React.Dispatch<React.SetStateAction<string[]>>;
+  city: CityType;
+  cities: CityType[];
+  setCities: React.Dispatch<React.SetStateAction<CityType[]>>;
 }
 
 const City = (props: Iprops) => {
-  const [city, setCity] = useState<CityType>();
-  const [err, setError] = useState(false);
-
-  useEffect(() => {
-    const getCity = async () => {
-      try {
-        await fetchCity(props.city).then((res) => {
-          setCity(res.data);
-        });
-      } catch (error) {
-        setError(true);
-      }
-    };
-    getCity();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
   return (
     <>
-      {err ? (
-        <p className="err">Couldn't fetch the data for {props.city}</p>
-      ) : (
-        <div className="cityWrapper">
-          <div className="cityContainer">
-            <div className="split">
-              <div className="cityField">City name</div>
-              <div className="cityField">{city?.location.name}</div>
-            </div>
-            <div className="split">
-              <div className="cityField">Description of current weather</div>
-              <div className="cityField">{city?.current.condition.text}</div>
-            </div>
-            <div className="split">
-              <div className="cityField">Image of current weather</div>
-              <div className="cityField">{`${city?.current.condition.code}`}</div>
-            </div>
-            <div className="split">
-              <div className="cityField">Current temperature in C</div>
-              <div className="cityField">{`${city?.current.temp_c}`}</div>
-            </div>
-            <div className="split">
-              <div className="cityField">Current humidity</div>
-              <div className="cityField">{`${city?.current.humidity}`}</div>
-            </div>
-            <div className="split">
-              <div className="cityField">Current precipitation</div>
-              <div className="cityField">{`${city?.current.precip_mm}`}</div>
+      <div className="cityWrapper">
+        <div className="cityContainer">
+          <div className="split">
+            <div className="cityField">City name</div>
+            <div className="cityField">{props.city?.location.name}</div>
+          </div>
+          <div className="split">
+            <div className="cityField">Description of current weather</div>
+            <div className="cityField">
+              {props.city?.current.condition.text}
             </div>
           </div>
-          <FontAwesomeIcon
-            className="deleteBtn"
-            icon={faTrash}
-            onClick={() => {
-              props.setCities(
-                props.cities.filter((x) => x !== city?.location.name)
-              );
-            }}
-            title="Delete City From The List"
-          />
+          <div className="split">
+            <div className="cityField">Image of current weather</div>
+            <div className="cityField">
+              <img
+                src={props.city?.current.condition.icon}
+                alt="weather icon"
+                className="weatherIcon"
+              />
+            </div>
+          </div>
+          <div className="split">
+            <div className="cityField">Current temperature in &#8451;</div>
+            <div className="cityField">
+              {`${props.city?.current.temp_c}`}&#8451;
+            </div>
+          </div>
+          <div className="split">
+            <div className="cityField">Current humidity</div>
+            <div className="cityField">
+              {`${props.city?.current.humidity}`}{" "}
+              <FontAwesomeIcon icon={faDroplet} />
+            </div>
+          </div>
+          <div className="split">
+            <div className="cityField">Current precipitation</div>
+            <div className="cityField">
+              {`${props.city?.current.precip_mm}`}{" "}
+              <FontAwesomeIcon icon={faCloudRain} />
+            </div>
+          </div>
         </div>
-      )}
+        <FontAwesomeIcon
+          className="deleteBtn"
+          icon={faTrash}
+          onClick={() => {
+            props.setCities(
+              props.cities.filter(
+                (x) => x.location.name !== props.city?.location.name
+              )
+            );
+          }}
+          title="Delete City From The List"
+        />
+      </div>
     </>
   );
 };
