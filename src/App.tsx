8b71039deaa1c0c4e35c2c386711c4d3dc53407d
Kieszku94 <kieszku94@gmail.com>
2023-04-06 08:07:28 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import { fetchCity } from "./helpers/fetchCity";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "./components/Home";
-import Login from "./components/Login";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import { useUserAuthenticated } from "./context/userContext";
 
 function App() {
-  const cities = ["Moscow", "London"];
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  useEffect(() => {
-    cities.map(async (city) => {
-      try {
-        let result = (await fetchCity(city)).data;
-        console.log(result);
-      } catch (error) {
-        console.log(error);
-      }
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const isAuthenticated = useUserAuthenticated();
+
+  // useEffect(() => {
+  //   cities.map(async (city) => {
+  //     try {
+  //       let result = (await fetchCity(city)).data;
+  //       console.log(result);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   });
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   return (
     <Routes>
