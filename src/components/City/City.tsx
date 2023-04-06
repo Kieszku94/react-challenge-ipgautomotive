8a6b711c04d66ae6bbccfd3ccb87522bf3104562
Kieszku94 <kieszku94@gmail.com>
2023-04-06 16:27:03 +0100

    Update City.tsx

--- src/components/City/City.tsx
+++ src/components/City/City.tsx
@@ -13,6 +13,7 @@ interface Iprops {
 
 const City = (props: Iprops) => {
   const [city, setCity] = useState<CityType>();
+  const [err, setError] = useState(false);
 
   useEffect(() => {
     const getCity = async () => {
@@ -21,51 +22,57 @@ const City = (props: Iprops) => {
           setCity(res.data);
         });
       } catch (error) {
-        console.log(error);
+        setError(true);
       }
     };
     getCity();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
-    <div className="cityWrapper">
-      <div className="cityContainer">
-        <div className="split">
-          <div className="cityField">City name</div>
-          <div className="cityField">{city?.location.name}</div>
+    <>
+      {err ? (
+        <p className="err">Couldn't fetch the data for {props.city}</p>
+      ) : (
+        <div className="cityWrapper">
+          <div className="cityContainer">
+            <div className="split">
+              <div className="cityField">City name</div>
+              <div className="cityField">{city?.location.name}</div>
+            </div>
+            <div className="split">
+              <div className="cityField">Description of current weather</div>
+              <div className="cityField">{city?.current.condition.text}</div>
+            </div>
+            <div className="split">
+              <div className="cityField">Image of current weather</div>
+              <div className="cityField">{`${city?.current.condition.code}`}</div>
+            </div>
+            <div className="split">
+              <div className="cityField">Current temperature in C</div>
+              <div className="cityField">{`${city?.current.temp_c}`}</div>
+            </div>
+            <div className="split">
+              <div className="cityField">Current humidity</div>
+              <div className="cityField">{`${city?.current.humidity}`}</div>
+            </div>
+            <div className="split">
+              <div className="cityField">Current precipitation</div>
+              <div className="cityField">{`${city?.current.precip_mm}`}</div>
+            </div>
+          </div>
+          <FontAwesomeIcon
+            className="deleteBtn"
+            icon={faTrash}
+            onClick={() => {
+              props.setCities(
+                props.cities.filter((x) => x !== city?.location.name)
+              );
+            }}
+            title="Delete City From The List"
+          />
         </div>
-        <div className="split">
-          <div className="cityField">Description of current weather</div>
-          <div className="cityField">{city?.current.condition.text}</div>
-        </div>
-        <div className="split">
-          <div className="cityField">Image of current weather</div>
-          <div className="cityField">{`${city?.current.condition.code}`}</div>
-        </div>
-        <div className="split">
-          <div className="cityField">Current temperature in C</div>
-          <div className="cityField">{`${city?.current.temp_c}`}</div>
-        </div>
-        <div className="split">
-          <div className="cityField">Current humidity</div>
-          <div className="cityField">{`${city?.current.humidity}`}</div>
-        </div>
-        <div className="split">
-          <div className="cityField">Current precipitation</div>
-          <div className="cityField">{`${city?.current.precip_mm}`}</div>
-        </div>
-      </div>
-      <FontAwesomeIcon
-        className="deleteBtn"
-        icon={faTrash}
-        onClick={() => {
-          props.setCities(
-            props.cities.filter((x) => x !== city?.location.name)
-          );
-        }}
-        title="Delete City From The List"
-      />
-    </div>
+      )}
+    </>
   );
 };
 
