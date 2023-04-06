478ae9e2c96cbf05ea6d921c65d60dc06b153a9f
Kieszku94 <kieszku94@gmail.com>
2023-04-06 15:34:46 +0100

    Update getCitiesList.ts

--- src/helpers/getCitiesList.ts
+++ src/helpers/getCitiesList.ts
@@ -5,7 +5,6 @@ export const getCitiesList = (textboxValue: string): string[] => {
   for (let i = 0; i < citiesList.length; i++) {
     citiesList[i] = citiesList[i].trim();
     citiesList[i] = toPascalCase(citiesList[i]);
-    console.log(citiesList[i]);
   }
   return citiesList;
 };
