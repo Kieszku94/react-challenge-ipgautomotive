03586257126076af5647df7948c077e0d15c9dba
Kieszku94 <kieszku94@gmail.com>
2023-04-06 23:59:44 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import { fetchCity } from "./helpers/fetchCity";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
@@ -8,18 +7,6 @@ import { useUserAuthenticated } from "./context/userContext";
 
 function App() {
   const isAuthenticated = useUserAuthenticated();
-
-  // useEffect(() => {
-  //   cities.map(async (city) => {
-  //     try {
-  //       let result = (await fetchCity(city)).data;
-  //       console.log(result);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   });
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
 
   return (
     <Routes>
