baa35934837ebbdba7669b2a37aacbada2731bdf
Kieszku94 <kieszku94@gmail.com>
2023-04-06 15:38:58 +0100

    Create types.ts

---
+++ src/types/types.ts
@@ -0,0 +1,22 @@
+export type Condition = {
+  code: Number;
+  text: string;
+  icon: string;
+};
+
+export type Location = {
+  name: String;
+};
+
+export type Current = {
+  condition: Condition;
+  temp_c: Number;
+  humidity: Number;
+  precip_mm: Number;
+};
+
+export type CityType = {
+  location: Location;
+  current: Current;
+  id: Number;
+};
